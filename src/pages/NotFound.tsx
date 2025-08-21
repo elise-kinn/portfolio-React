e451730710d22458e69fef2162f8abea78a7a19b
Elise L <elli3183@gmail.com>
2025-08-21 14:44:57 +0200

    Add : Not Found Page

--- src/pages/NotFound.tsx
+++ src/pages/NotFound.tsx
@@ -1,8 +1,11 @@
+import Hero from "../components/global/hero"
+
 const NotFound = () => {
     return (
-        <header>
-            
-        </header>
+        <>
+        <Hero title="Page introuvable"/>
+        <p id="not-found">La page que vous souhaitez consuulter ne semble pas exister :(</p>
+        </>
     )
 }
 
